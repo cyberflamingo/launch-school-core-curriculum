e4bd4df9f3f12e0f159e919009fdc684e2c1685e
cyberflamingo <19971275+cyberflamingo@users.noreply.github.com>
2020-05-09 12:39:40 +0900

    Add PEDAC template for future use

---
+++ template_pedac_lite.rb
@@ -0,0 +1,97 @@
+# frozen_string_literal: true
+
+=begin rdoc
+
+= PEDAC Template
+
+== (Understand the) Problem
+
+First, identify the inputs and the outputs of the problem
+
+* Inputs:
+  1.
+  2.
+* Output:
+  1.
+
+---
+
+*Problem Domain:*
+
+
+---
+
+*Implicit Requirements:*
+
+
+---
+
+*Clarifying Questions:*
+
+1.
+2.
+3.
+
+---
+
+*Mental Model:*
+
+
+---
+
+== Examples / Test Cases / Edge Cases
+
+*Examples:*
+
+* Example 1
+  * Inputs:
+    1.
+    2.
+  * Output
+    1.
+* Example 2
+  * Inputs:
+    1.
+    2.
+  * Output
+    1.
+
+---
+
+*Your Test Cases:*
+
+* Example 3
+  * Inputs:
+    1.
+    2.
+  * Output
+    1.
+
+---
+
+*Your Edge Cases:*
+
+* Example 4
+  * Inputs:
+    1.
+    2.
+  * Output
+    1.
+
+---
+
+== Data Structure
+
+*Your Data Structure:*
+
+
+---
+
+== Algorithm
+
+*Your Algorith:*
+
+
+== Code
+
+=end
