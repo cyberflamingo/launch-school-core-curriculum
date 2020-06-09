ee27ca8442d4952ec6ef6838d51e9cbbebe20e7e
cyberflamingo <19971275+cyberflamingo@users.noreply.github.com>
2020-06-09 09:08:49 +0900

    Keep score of each player in the game

--- 02_small_programs/rock_paper_scissors/rock_paper_scissors_lizard_spock.rb
+++ 02_small_programs/rock_paper_scissors/rock_paper_scissors_lizard_spock.rb
@@ -14,7 +14,7 @@ def find_associated_string(letter)
   associated_string[0]
 end
 
-def display_results(player, computer)
+def determine_winner(player, computer, scores)
   pattern = [player, computer]
   winning_patterns = [['scissors', 'paper'], ['paper', 'rock'],
                       ['rock', 'lizard'], ['lizard', 'Spock'],
@@ -23,10 +23,31 @@ def display_results(player, computer)
                       ['Spock', 'rock'], ['rock', 'scissors']]
   loosing_patterns = winning_patterns.map(&:reverse)
 
-  return 'You won!' if winning_patterns.include?(pattern)
-  return 'Computer won!' if loosing_patterns.include?(pattern)
-  "It's a tie!"
+  winner = 'none'
+  if winning_patterns.include?(pattern)
+    winner = 'player'
+    scores[:player] += 1
+  elsif loosing_patterns.include?(pattern)
+    winner = 'computer'
+    scores[:computer] += 1
+  end
+
+  winner
 end
+
+def display_results(winner_name)
+  case winner_name
+  when 'player'
+    'You won!'
+  when 'computer'
+    'Computer won!'
+  else
+    "It's a tie!"
+  end
+end
+
+scores = { player: 0,
+           computer: 0 }
 
 loop do
   choice = ''
@@ -48,11 +69,31 @@ loop do
 
   prompt("You chose: #{choice}; Computer chose: #{computer_choice}.")
 
-  puts display_results(choice, computer_choice)
+  winner = determine_winner(choice, computer_choice, scores)
+  puts display_results(winner)
 
-  prompt('Do you want to play again?')
-  answer = Kernel.gets().chomp()
-  break unless answer.downcase().start_with?('y')
+  prompt("Current score: \n" \
+         "|  Player  | Computer | \n" \
+         "| ======== | ======== | \n" \
+         "|       #{scores[:player]}  |       #{scores[:computer]}  |")
+
+  grand_winner = if scores[:player] == 5
+                   'player'
+                 elsif scores[:computer] == 5
+                   'computer'
+                 else
+                   false
+                 end
+
+  if grand_winner
+    prompt("The grand winner is #{grand_winner}! Congratulations!")
+
+    scores = { player: 0,
+               computer: 0 }
+    prompt('Do you want to play again?')
+    answer = Kernel.gets().chomp()
+    break unless answer.downcase().start_with?('y')
+  end
 end
 
 prompt('Thank you for playing. Good bye!')
