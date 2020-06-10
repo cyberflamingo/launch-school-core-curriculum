3cf53f32cdb20365abff68f88349d5b97f5914dd
cyberflamingo <19971275+cyberflamingo@users.noreply.github.com>
2020-06-11 08:13:53 +0900

    Easy 2 Problems 4

---
+++ small_problems/easy_2/04_when_will_i_retire.rb
@@ -0,0 +1,16 @@
+require 'date'
+
+puts 'What is your age?'
+age = gets.to_i
+
+puts 'At what age would you like to retire?'
+retire_age = gets.to_i
+
+CURRENT_YEAR = DateTime.now.year
+
+work_years = retire_age - age
+
+retire_year = CURRENT_YEAR + work_years
+
+puts "It's #{CURRENT_YEAR}. You will retire in #{retire_year}."
+puts "You have only #{work_years} years of work to go!"
