ae5ccd91d7e42448c912a725b27275957888d8e7
cyberflamingo <19971275+cyberflamingo@users.noreply.github.com>
2020-06-13 14:26:32 +0900

    Refactore RPSLS

--- 02_small_programs/rock_paper_scissors/rock_paper_scissors_lizard_spock.rb
+++ 02_small_programs/rock_paper_scissors/rock_paper_scissors_lizard_spock.rb
@@ -1,7 +1,14 @@
+require 'pry'
 VALID_CHOICES = %w(rock paper scissors lizard Spock)
+WINNING_PATTERNS = [['scissors', 'paper'], ['paper', 'rock'],
+                    ['rock', 'lizard'], ['lizard', 'Spock'],
+                    ['Spock', 'scissors'], ['scissors', 'lizard'],
+                    ['lizard', 'paper'], ['paper', 'Spock'],
+                    ['Spock', 'rock'], ['rock', 'scissors']]
+LOSING_PATTERNS = WINNING_PATTERNS.map(&:reverse)
 
 def prompt(message)
-  Kernel.puts("=> #{message}")
+  Kernel.puts("\n" + "=> #{message}")
 end
 
 def find_associated_string(letter)
@@ -14,42 +21,7 @@ def find_associated_string(letter)
   associated_string[0]
 end
 
-def determine_winner(player, computer, scores)
-  pattern = [player, computer]
-  winning_patterns = [['scissors', 'paper'], ['paper', 'rock'],
-                      ['rock', 'lizard'], ['lizard', 'Spock'],
-                      ['Spock', 'scissors'], ['scissors', 'Lizard'],
-                      ['lizard', 'paper'], ['paper', 'Spock'],
-                      ['Spock', 'rock'], ['rock', 'scissors']]
-  loosing_patterns = winning_patterns.map(&:reverse)
-
-  winner = 'none'
-  if winning_patterns.include?(pattern)
-    winner = 'player'
-    scores[:player] += 1
-  elsif loosing_patterns.include?(pattern)
-    winner = 'computer'
-    scores[:computer] += 1
-  end
-
-  winner
-end
-
-def display_results(winner_name)
-  case winner_name
-  when 'player'
-    'You won!'
-  when 'computer'
-    'Computer won!'
-  else
-    "It's a tie!"
-  end
-end
-
-scores = { player: 0,
-           computer: 0 }
-
-loop do
+def player_move
   choice = ''
   loop do
     prompt("Choose one by typing its first letter ('S' majuscule for " \
@@ -64,35 +36,95 @@ loop do
       prompt('Not a valid choice.')
     end
   end
+  choice
+end
 
-  computer_choice = VALID_CHOICES.sample
+def computer_move
+  VALID_CHOICES.sample
+end
 
-  prompt("You chose: #{choice}; Computer chose: #{computer_choice}.")
+def display_choices(player, computer)
+  prompt("You chose: #{player}; Computer chose: #{computer}.")
+end
 
-  winner = determine_winner(choice, computer_choice, scores)
-  puts display_results(winner)
+def determine_winner(player, computer)
+  pattern = [player, computer]
 
+  winner = 'tie'
+  if WINNING_PATTERNS.include?(pattern)
+    winner = 'player'
+  elsif LOSING_PATTERNS.include?(pattern)
+    winner = 'computer'
+  end
+  winner
+end
+
+def update_scores(winner_name, scores_table)
+  return if winner_name == 'tie'
+  scores_table[winner_name.to_sym] += 1
+end
+
+def display_results(winner_name)
+  case winner_name
+  when 'player'
+    'You won!'
+  when 'computer'
+    'Computer won!'
+  else
+    "It's a tie!"
+  end
+end
+
+def display_scores(scores)
   prompt("Current score: \n" \
          "|  Player  | Computer | \n" \
          "| ======== | ======== | \n" \
          "|       #{scores[:player]}  |       #{scores[:computer]}  |")
+end
 
-  grand_winner = if scores[:player] == 5
-                   'player'
-                 elsif scores[:computer] == 5
-                   'computer'
+def determine_grand_winner(winner_name, scores_table)
+  grand_winner = if scores_table[winner_name.to_sym] == 5
+                   winner_name
                  else
                    false
                  end
+  grand_winner
+end
 
+def display_grand_winner(winner_name)
+  prompt("The grand winner is #{winner_name}! Congratulations!")
+end
+
+def play_again?
+  prompt('Do you want to play again?')
+  answer = Kernel.gets().chomp()
+  system 'clear'
+  answer
+end
+
+scores = { player: 0,
+           computer: 0 }
+
+system 'clear'
+
+loop do
+  player_choice = player_move
+  computer_choice = computer_move
+  display_choices(player_choice, computer_choice)
+  winner = determine_winner(player_choice, computer_choice)
+  update_scores(winner, scores)
+  puts display_results(winner)
+  display_scores(scores)
+
+  grand_winner = determine_grand_winner(winner, scores)
   if grand_winner
-    prompt("The grand winner is #{grand_winner}! Congratulations!")
+    display_grand_winner(grand_winner)
 
     scores = { player: 0,
                computer: 0 }
-    prompt('Do you want to play again?')
-    answer = Kernel.gets().chomp()
-    break unless answer.downcase().start_with?('y')
+    continue = play_again?
+
+    break unless continue.downcase().start_with?('y')
   end
 end
 
