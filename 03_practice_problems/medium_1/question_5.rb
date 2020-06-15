b09df2d9b787de9efab04d5011d4ef75219a7941
cyberflamingo <19971275+cyberflamingo@users.noreply.github.com>
2020-06-15 09:06:42 +0900

    Lession 3 Meidum 1

---
+++ 03_practice_problems/medium_1/question_5.rb
@@ -0,0 +1,13 @@
+limit = 15
+
+def fib(first_num, second_num, limit)
+  while first_num + second_num < limit
+    sum = first_num + second_num
+    first_num = second_num
+    second_num = sum
+  end
+  sum
+end
+
+result = fib(0, 1, limit)
+puts "result is #{result}"
