14679ceb1b5b6a683dab7a33ba1b36a21706e554
cyberflamingo <19971275+cyberflamingo@users.noreply.github.com>
2020-06-17 22:17:36 +0900

    Easy 3 Problem 2

---
+++ small_problems/easy_3/02_arithmetic_integer.rb
@@ -0,0 +1,12 @@
+puts "==> Enter the first number:"
+first_num = gets.to_i
+
+puts "==> Enter the second number:"
+second_num = gets.to_i
+
+puts "==> #{first_num} + #{second_num} = #{first_num + second_num}"
+puts "==> #{first_num} - #{second_num} = #{first_num - second_num}"
+puts "==> #{first_num} * #{second_num} = #{first_num * second_num}"
+puts "==> #{first_num} / #{second_num} = #{first_num / second_num}"
+puts "==> #{first_num} % #{second_num} = #{first_num % second_num}"
+puts "==> #{first_num} ** #{second_num} = #{first_num**second_num}"
