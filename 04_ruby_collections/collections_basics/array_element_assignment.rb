5feba857cd0c808d9fac8a1437dba10bc817dc22
cyberflamingo <19971275+cyberflamingo@users.noreply.github.com>
2020-06-27 19:31:51 +0900

    Lesson 4

---
+++ 04_ruby_collections/collections_basics/array_element_assignment.rb
@@ -0,0 +1,7 @@
+arr = [1, 2, 3, 4, 5]
+
+arr2 = arr.map do |val|
+  val + 1
+end
+
+p arr2
---
+++ 04_ruby_collections/collections_basics/hash_element_assignment.rb
@@ -0,0 +1,11 @@
+hsh = { apple: 'Produce',
+        carrot: 'Produce',
+        pear: 'Produce',
+        broccoli: 'Produce' }
+
+hsh[:apple] = 'Fruit'
+hsh[:carrot] = 'Vegetable'
+hsh[:pear] = 'Fruit'
+hsh[:broccoli] = 'Vegetable'
+
+p hsh
