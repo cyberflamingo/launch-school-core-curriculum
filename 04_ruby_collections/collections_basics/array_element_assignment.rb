cf5caf7eb95116840e9e0773df348b5ab792c135
cyberflamingo <19971275+cyberflamingo@users.noreply.github.com>
2020-07-03 08:56:39 +0900

    Deleted folders

--- 04_ruby_collections/collections_basics/array_element_assignment.rb
+++
@@ -1,7 +0,0 @@
-arr = [1, 2, 3, 4, 5]
-
-arr2 = arr.map do |val|
-  val + 1
-end
-
-p arr2
--- 04_ruby_collections/collections_basics/element_assignment.rb
+++
@@ -1,8 +0,0 @@
-str = "joe's favorite color is blue"
-arr = str.split
-
-arr.each do |val|
-  val[0] = val[0].upcase!
-end
-
-puts arr.join(' ')
