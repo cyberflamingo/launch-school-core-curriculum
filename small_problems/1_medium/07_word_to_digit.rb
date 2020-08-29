4e1cea2c19855bf6e1564c29d8e76fab1f9760c1
cyberflamingo <19971275+cyberflamingo@users.noreply.github.com>
2020-08-29 14:37:14 +0900

    Replace solution of 07 with regex
    The current solution has the disaventage of replacing every occurence of the word
    even part of other words. This solution eliminate this error.

--- small_problems/1_medium/07_word_to_digit.rb
+++ small_problems/1_medium/07_word_to_digit.rb
@@ -2,11 +2,11 @@ DIGITS = %w(zero one two three four five six seven eight nine)
 
 def word_to_digit(string)
   DIGITS.each_with_index do |digit, index|
-    string.gsub!(digit, index.to_s)
+    string.gsub!(/\b#{digit}\b/, index.to_s)
   end
 
   string
 end
 
-p word_to_digit('Please call me at five five five one two three four. ' \
-                'Thanks.') == 'Please call me at 5 5 5 1 2 3 4. Thanks.'
+p word_to_digit('Please call freight me at five five five one two three four. ' \
+                'Thanks.') #== 'Please call me at 5 5 5 1 2 3 4. Thanks.'
