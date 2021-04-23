2bf79cfc4ff965b1be01b750de7b5fcc6dd5e37f
Alexandre Mercier <alex@cyberflamingo.net>
2021-04-23 14:03:37 +0900

    Add Person class with operators methods

---
+++ RB120/03_more_oo_ruby/person.rb
@@ -0,0 +1,57 @@
+class Person
+  attr_accessor :name, :age
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+
+  def >(other_person)
+    age > other_person.age
+  end
+end
+
+class Team
+  attr_accessor :name, :members
+
+  def initialize(name)
+    @name = name
+    @members = []
+  end
+
+  def <<(person)
+    members.push person
+  end
+
+  def +(other_team)
+    temp_team = Team.new("Temporary Team")
+    temp_team.members = members + other_team.members
+    temp_team
+  end
+
+  def [](idx)
+    members[idx]
+  end
+
+  def []=(idx, obj)
+    members[idx] = obj
+  end
+end
+
+cowboys = Team.new("Dallas Cowboys")
+cowboys << Person.new("Troy Aikman", 48)
+cowboys << Person.new("Emmitt Smith", 46)
+cowboys << Person.new("Michael Irvin", 49)
+
+niners = Team.new("San Francisco 49ers")
+niners << Person.new("Joe Montana", 59)
+niners << Person.new("Jerry Rice", 52)
+niners << Person.new("Deion Sanders", 47)
+
+dream_team = cowboys + niners
+dream_team.name = "Dream Team"
+
+dream_team[4]
+dream_team[5] = Person.new("JJ", 72)
+
+puts dream_team.inspect
