c074e5853ab62ca12fb3f01a46729e8f6cee747e
Alexandre Mercier <alex@cyberflamingo.net>
2021-06-15 21:57:45 +0900

    Add new select method to TodoList

---
+++ RB130/01_blocks/todolist_select.rb
@@ -0,0 +1,149 @@
+# This class represents a todo item and its associated
+# data: name and description. There's also a "done"
+# flag to show whether this todo item is done.
+
+class Todo
+  DONE_MARKER = 'X'
+  UNDONE_MARKER = ' '
+
+  attr_accessor :title, :description, :done
+
+  def initialize(title, description='')
+    @title = title
+    @description = description
+    @done = false
+  end
+
+  def done!
+    self.done = true
+  end
+
+  def done?
+    done
+  end
+
+  def undone!
+    self.done = false
+  end
+
+  def to_s
+    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  end
+
+  def ==(other_todo)
+    title == other_todo.title &&
+      description == other_todo.description &&
+      done == other_todo.done
+  end
+end
+
+# This class represents a collection of Todo objects.
+# You can perform typical collection-oriented actions
+# on a TodoList object, including iteration and selection.
+
+class TodoList
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
+
+  # rest of class needs implementation
+  def add(todo)
+    raise TypeError, 'Can only add Todo objects' unless todo.instance_of? Todo
+    todos << todo
+  end
+
+  alias << add
+
+  def size
+    todos.size
+  end
+
+  def first
+    todos.first
+  end
+
+  def last
+    todos.last
+  end
+
+  def to_a
+    todos.clone
+  end
+
+  def done?
+    todos.all?(&:done?)
+  end
+
+  def item_at(index)
+    todos.fetch(index)
+  end
+
+  def mark_done_at(index)
+    todos[index].done!
+  end
+
+  def mark_undone_at(index)
+    todos[index].undone!
+  end
+
+  def done!
+    todos.each(&:done!)
+  end
+
+  def shift
+    todos.shift
+  end
+
+  def pop
+    todos.pop
+  end
+
+  def remove_at(index)
+    todos.delete_at(index)
+  end
+
+  def to_s
+    text = "---- #{title} ----\n"
+    text << @todos.map(&:to_s).join("\n")
+    text
+  end
+
+  def each
+    todos.each do |todo|
+      yield(todo)
+    end
+
+    self
+  end
+
+  def select
+    selection = TodoList.new(title)
+    each do |todo|
+      selection.add(todo) if yield(todo)
+    end
+
+    selection
+  end
+
+  private
+
+  attr_accessor :todos
+end
+
+todo1 = Todo.new("Buy milk")
+todo2 = Todo.new("Clean room")
+todo3 = Todo.new("Go to gym")
+
+list = TodoList.new("Today's Todos")
+list.add(todo1)
+list.add(todo2)
+list.add(todo3)
+
+todo1.done!
+
+results = list.select(&:done?)
+
+puts results.inspect
