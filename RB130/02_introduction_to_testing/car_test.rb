c7f8fd21440d49467274bdccbbab2317988c1cb8
Alexandre Mercier <alex@cyberflamingo.net>
2021-06-25 09:18:20 +0900

    Use SEAT approach to refactor car test

--- RB130/02_introduction_to_testing/car_test.rb
+++ RB130/02_introduction_to_testing/car_test.rb
@@ -5,25 +5,25 @@ Minitest::Reporters.use!
 require_relative 'car'
 
 class CarTest < MiniTest::Test
+  def setup
+    @car = Car.new
+  end
+
   def test_car_exists
-    car = Car.new
-    assert(car)
+    assert(@car)
   end
 
   def test_wheels
-    car = Car.new
-    assert_equal(4, car.wheels)
+    assert_equal(4, @car.wheels)
   end
 
   # def test_bad_wheels
   #   skip
-  #   car = Car.new
-  #   assert_equal(3, car.wheels)
+  #   assert_equal(3, @car.wheels)
   # end
 
   def test_name_is_nil
-    car = Car.new
-    assert_nil(car.name)
+    assert_nil(@car.name)
   end
 
   def test_raise_initialize_with_arg
@@ -36,16 +36,14 @@ class CarTest < MiniTest::Test
   end
 
   def test_instance_of_car
-    car = Car.new
-    assert_instance_of(Car, car)
+    assert_instance_of(Car, @car)
   end
 
   def test_includes_car
-    car = Car.new
     arr = [1, 2, 3]
-    arr << car
+    arr << @car
 
-    assert_includes(arr, car)
+    assert_includes(arr, @car)
   end
 
   # assert_includes calls assert_equal in its implementation, and Minitest
