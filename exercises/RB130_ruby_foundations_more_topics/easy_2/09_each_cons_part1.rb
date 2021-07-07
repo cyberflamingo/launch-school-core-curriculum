4c0a0a720fb548431613d9563a55605203e05e3d
Alexandre Mercier <alex@cyberflamingo.net>
2021-07-08 08:53:18 +0900

    Fix exercise 9 to show true for each proposition

--- exercises/RB130_ruby_foundations_more_topics/easy_2/09_each_cons_part1.rb
+++ exercises/RB130_ruby_foundations_more_topics/easy_2/09_each_cons_part1.rb
@@ -1,14 +1,16 @@
-def each_cons(collection)
-  collection.each_with_index do |_, index|
-    yield(collection[index], collection[index + 1])
+def each_cons(ary)
+  ary.each_with_index do |_, index|
+    yield(ary[index], ary[index + 1]) unless (index + 1) >= ary.size
   end
+
+  nil
 end
 
 hash = {}
 result = each_cons([1, 3, 6, 10]) do |value1, value2|
   hash[value1] = value2
 end
-p result == nil
+p result.nil?
 p hash == { 1 => 3, 3 => 6, 6 => 10 }
 
 hash = {}
@@ -16,11 +18,11 @@ result = each_cons([]) do |value1, value2|
   hash[value1] = value2
 end
 p hash == {}
-p result == nil
+p result.nil?
 
 hash = {}
 result = each_cons(['a', 'b']) do |value1, value2|
   hash[value1] = value2
 end
-p hash == {'a' => 'b'}
-p result == nil
+p hash == { 'a' => 'b' }
+p result.nil?
