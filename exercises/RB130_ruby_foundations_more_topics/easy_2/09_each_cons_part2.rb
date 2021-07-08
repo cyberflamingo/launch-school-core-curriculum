665c4195e6d6bc3ca29a9dc6e0a4ef11fd4d9ad4
Alexandre Mercier <alex@cyberflamingo.net>
2021-07-08 09:58:34 +0900

    Add part 2 or exercise 09

---
+++ exercises/RB130_ruby_foundations_more_topics/easy_2/09_each_cons_part2.rb
@@ -0,0 +1,40 @@
+def each_cons(array, num)
+  num -= 1
+
+  array.each_index do |index|
+    break if index + num >= array.size
+    yield(*array[index..(index + num)])
+  end
+
+  nil
+end
+
+hash = {}
+each_cons([1, 3, 6, 10], 1) do |value|
+  hash[value] = true
+end
+p hash == { 1 => true, 3 => true, 6 => true, 10 => true }
+
+hash = {}
+each_cons([1, 3, 6, 10], 2) do |value1, value2|
+  hash[value1] = value2
+end
+p hash == { 1 => 3, 3 => 6, 6 => 10 }
+
+hash = {}
+each_cons([1, 3, 6, 10], 3) do |value1, *values|
+  hash[value1] = values
+end
+p hash == { 1 => [3, 6], 3 => [6, 10] }
+
+hash = {}
+each_cons([1, 3, 6, 10], 4) do |value1, *values|
+  hash[value1] = values
+end
+p hash == { 1 => [3, 6, 10] }
+
+hash = {}
+each_cons([1, 3, 6, 10], 5) do |value1, *values|
+  hash[value1] = values
+end
+p hash == {}
