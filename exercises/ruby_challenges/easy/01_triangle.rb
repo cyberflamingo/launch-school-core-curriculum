40afa78dc7b9c51e8074408d300903b1c9b1f2e3
Alexandre Mercier <alex@cyberflamingo.net>
2021-07-13 08:50:16 +0900

    Refactor Triangle class for better lisibility

--- exercises/ruby_challenges/easy/01_triangle.rb
+++ exercises/ruby_challenges/easy/01_triangle.rb
@@ -2,31 +2,28 @@ class Triangle
   attr_reader :kind
 
   def initialize(s1, s2, s3)
-    check_for_side_error([s1, s2, s3])
-    @side1 = s1
-    @side2 = s2
-    @side3 = s3
+    check_validity([s1, s2, s3])
     @kind = find_kind([s1, s2, s3])
   end
 
   private
 
   def find_kind(sides)
-    return 'equilateral' if sides.all?(sides[0])
-
-    if sides.sort[0..1].reduce(:-).zero? || sides.sort[1..-1].reduce(:-).zero?
-      return 'isosceles'
+    if sides.uniq.size == 1
+      'equilateral'
+    elsif sides.uniq.size == 2
+      'isosceles'
+    else
+      'scalene'
     end
-
-    'scalene'
   end
 
-  def check_for_side_error(sides)
+  def check_validity(sides)
     sum = sides.reduce(:*)
-    negative = sides.select(&:negative?)
+    negative_sides = sides.select(&:negative?)
     is_impossible = sides.min(2).reduce(:+) <= sides.sort.max
 
-    if sum <= 0 || negative.count > 0 || is_impossible
+    if sum <= 0 || negative_sides.count > 0 || is_impossible
       raise ArgumentError, 'Impossible triangle'
     end
   end
