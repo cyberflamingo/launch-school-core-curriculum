769e81f5089f42475ffce0b3467b4aba8c6503ef
Alexandre Mercier <alex@cyberflamingo.net>
2022-02-13 18:50:49 +0900

    Add layout template to book viewer

--- RB175/03_working_with_sinatra/book_viewer/book_viewer.rb
+++ RB175/03_working_with_sinatra/book_viewer/book_viewer.rb
@@ -2,17 +2,26 @@ require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
 
+before do
+  @toc = File.readlines("data/toc.txt")
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @toc = File.readlines("data/toc.txt")
 
   erb :home
 end
 
-get "/chapter/:number" do |num|
-  @title = "Chapter #{num}"
-  @toc = File.readlines("data/toc.txt")
-  @chapter = File.read("data/chp#{num}.txt")
+get "/chapters/:number" do
+  number = params[:number].to_i
+  chapter_name = @toc[number - 1]
+  @title = "Chapter #{number}: #{chapter_name}"
+
+  @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
+end
+
+get "/show/:name" do
+  params[:name]
 end
