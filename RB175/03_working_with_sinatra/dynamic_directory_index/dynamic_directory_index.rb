b8e654db8286dc8b76fcca7489070556a31abdc4
Alexandre Mercier <alex@cyberflamingo.net>
2022-02-13 18:09:00 +0900

    Add Dynamic Directory Index Challenge

---
+++ RB175/03_working_with_sinatra/dynamic_directory_index/dynamic_directory_index.rb
@@ -0,0 +1,21 @@
+require 'pry'
+require "sinatra"
+require "sinatra/reloader"
+
+get "/" do
+  @static_files = Dir.glob(:public.to_s + "/*").map { |file|
+    File.basename(file)
+  }.sort
+
+  @sort_url = "/?sort=des"
+  @sort_link = "Sort descending"
+
+  if params['sort'] == 'des'
+    @static_files.reverse!
+
+    @sort_url = "/?sort=asc"
+    @sort_link = "Sort ascending"
+  end
+
+  erb :index
+end
