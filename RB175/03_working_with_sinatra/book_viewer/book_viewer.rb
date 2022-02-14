f302bf076e44278be2a38e297a732a549e085cba
Alexandre Mercier <alex@cyberflamingo.net>
2022-02-14 10:04:15 +0900

    Add search capability

--- RB175/03_working_with_sinatra/book_viewer/book_viewer.rb
+++ RB175/03_working_with_sinatra/book_viewer/book_viewer.rb
@@ -24,6 +24,19 @@ get "/chapters/:number" do
   erb :chapter
 end
 
+get "/search" do
+  @results = chapters_matching(params[:query])
+  erb :search
+end
+
+get "/show/:name" do
+  params[:name]
+end
+
+not_found do
+  redirect "/"
+end
+
 helpers do
   def in_paragraphs(text)
     text.split("\n\n").map do |paragraph|
@@ -31,6 +44,26 @@ helpers do
     end.join end
 end
 
-not_found do
-  redirect "/"
+# Calls the block for each chapter, passing that chapter's number, name, and
+# contents.
+def each_chapter
+  @contents.each_with_index do |name, index|
+    number = index + 1
+    contents = File.read("data/chp#{number}.txt")
+    yield number, name, contents
+  end
+end
+
+# This method returns an Array of Hashes representing chapters that match the
+# specified query. Each Hash contain values for its :name and :number keys.
+def chapters_matching(query)
+  results = []
+
+  return results if !query || query.empty?
+
+  each_chapter do |number, name, contents|
+    results << { number: number, name: name } if contents.include?(query)
+  end
+
+  results
 end
