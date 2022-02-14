516f1104b50af8676b2d7c2280d08afba994e03a
Alexandre Mercier <alex@cyberflamingo.net>
2022-02-14 10:03:55 +0900

    Add routing and filters to sinatra-based webapp

--- RB175/03_working_with_sinatra/book_viewer/book_viewer.rb
+++ RB175/03_working_with_sinatra/book_viewer/book_viewer.rb
@@ -3,7 +3,7 @@ require "sinatra"
 require "sinatra/reloader"
 
 before do
-  @toc = File.readlines("data/toc.txt")
+  @contents = File.readlines("data/toc.txt")
 end
 
 get "/" do
@@ -14,14 +14,23 @@ end
 
 get "/chapters/:number" do
   number = params[:number].to_i
-  chapter_name = @toc[number - 1]
-  @title = "Chapter #{number}: #{chapter_name}"
+  chapter_name = @contents[number - 1]
 
+  redirect "/" unless (1..@contents.size).cover? number
+
+  @title = "Chapter #{number}: #{chapter_name}"
   @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
 end
 
-get "/show/:name" do
-  params[:name]
+helpers do
+  def in_paragraphs(text)
+    text.split("\n\n").map do |paragraph|
+      "<p>#{paragraph}</p>"
+    end.join end
+end
+
+not_found do
+  redirect "/"
 end
