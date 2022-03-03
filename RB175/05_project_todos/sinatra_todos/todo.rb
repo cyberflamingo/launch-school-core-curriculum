6026e8d86f96c1c457243dde6b0d0f209aa5806f
Alexandre Mercier <alex@cyberflamingo.net>
2022-03-03 09:52:44 +0900

    Add new feature to todo list

--- RB175/05_project_todos/sinatra_todos/todo.rb
+++ RB175/05_project_todos/sinatra_todos/todo.rb
@@ -8,6 +8,46 @@ configure do
   set :session_secret, 'ABSOLUTELY_NOT_A_GOOD_WAY_TO_SAVE_SECRET'
 end
 
+# rubocop:disable Metrics/BlockLength
+helpers do
+  def list_complete?(list)
+    todos_count(list) > 0 && todos_remaining_count(list) == 0
+  end
+
+  def list_class(list)
+    "complete" if list_complete?(list)
+  end
+
+  def todos_count(list)
+    list[:todos].size
+  end
+
+  def todos_remaining_count(list)
+    list[:todos].select { |todo| !todo[:completed] }.size
+  end
+
+  # rubocop:disable Lint/UnusedMethodArgument
+  def sort_lists(lists, &block)
+    complete_lists, incomplete_lists = lists.partition do |list|
+      list_complete?(list)
+    end
+
+    incomplete_lists.each { |list| yield list, lists.index(list) }
+    complete_lists.each { |list| yield list, lists.index(list) }
+  end
+
+  def sort_todos(todos, &block)
+    complete_todos, incomplete_todos = todos.partition do |todo|
+      todo[:completed]
+    end
+
+    incomplete_todos.each { |todo| yield todo, todos.index(todo) }
+    complete_todos.each { |todo| yield todo, todos.index(todo) }
+  end
+  # rubocop:enable Lint/UnusedMethodArgument
+end
+# rubocop:enable Metrics/BlockLength
+
 before do
   session[:lists] ||= []
 end
@@ -39,9 +79,9 @@ end
 
 # Return an error message if the name is invalid. Return nil if name is valid.
 def error_for_todo(name)
-  if !(1..100).cover? name.size
-    "Todo must be between 1 and 100 characters."
-  end
+  return unless !(1..100).cover? name.size
+
+  "Todo must be between 1 and 100 characters."
 end
 
 # Create a new list
@@ -121,4 +161,42 @@ post "/lists/:list_id/todos" do
 
     redirect "/lists/#{@list_id}"
   end
+end
+
+# Delete a todo from a list
+post '/lists/:list_id/todos/:id/destroy' do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+
+  todo_id = params[:id].to_i
+  @list[:todos].delete_at(todo_id)
+  session[:success] = "The todo has been deleted."
+
+  redirect "/lists/#{@list_id}"
+end
+
+# Update the status of a todo
+post "/lists/:list_id/todos/:id" do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+
+  todo_id = params[:id].to_i
+  is_completed = params[:completed] == "true"
+  @list[:todos][todo_id][:completed] = is_completed
+  session[:success] = "The todo has been updated."
+
+  redirect "/lists/#{@list_id}"
+end
+
+# Mark all todos as complete for a list
+post "/lists/:id/complete_all" do
+  @list_id = params[:id].to_i
+  @list = session[:lists][@list_id]
+
+  @list[:todos].each do |todo|
+    todo[:completed] = true
+  end
+
+  session[:success] = "All todos have been completed."
+  redirect "/lists/#{@list_id}"
 end
