c6985eaabcf72b26277a3f8f4d801f15174ba39a
Alexandre Mercier <alex@cyberflamingo.net>
2022-03-14 08:49:44 +0900

    Add Rack Test test folder

---
+++ RB175/08_project_file-based_cms/rack_test/test/app_test.rb
@@ -0,0 +1,21 @@
+ENV["RACK_ENV"] = "test"
+
+require "minitest/autorun"
+require "rack/test"
+
+require_relative "../app"
+
+class AppTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_index
+    get "/"
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_equal "Hello, world!", last_response.body
+  end
+end
