e7e05722a428e1ef7de3bec49fe0222c686982f5
Alexandre Mercier <alex@cyberflamingo.net>
2022-03-17 09:02:39 +0900

    Add Delete functionality

--- RB175/08_project_file-based_cms/cms/cms.rb
+++ RB175/08_project_file-based_cms/cms/cms.rb
@@ -97,3 +97,12 @@ post "/:filename" do
   session[:message] = "#{params[:filename]} has been updated."
   redirect "/"
 end
+
+post "/:filename/delete" do
+  file_path = File.join(data_path, params[:filename])
+
+  File.delete(file_path)
+
+  session[:message] = "#{params[:filename]} has been deleted."
+  redirect "/"
+end
--- RB175/08_project_file-based_cms/cms/test/cms_test.rb
+++ RB175/08_project_file-based_cms/cms/test/cms_test.rb
@@ -123,4 +123,18 @@ class CMSTest < Minitest::Test
     assert_equal 422, last_response.status
     assert_includes last_response.body, "A name is required"
   end
+
+  def test_deleting_document
+    create_document("test.txt")
+
+    post "/test.txt/delete"
+
+    assert_equal 302, last_response.status
+
+    get last_response["Location"]
+    assert_includes last_response.body, "test.txt has been deleted"
+
+    get "/"
+    refute_includes last_response.body, "test.txt"
+  end
 end
